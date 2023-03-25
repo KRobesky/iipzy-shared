1515762412bef5b7fe50af3340a26009f317e93f
ken Robesky <ken@robesky.org>
2023-03-25 05:36:07 -0700

    remove now_local

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,2 +1,2 @@
-const { local_hour_to_gmt_hour, now, now_local, set_timezoneOffsetMinutes} = require("./time_impl");
-module.exports = { local_hour_to_gmt_hour, now, now_local, set_timezoneOffsetMinutes };
+const { local_hour_to_gmt_hour, now, set_timezoneOffsetMinutes} = require("./time_impl");
+module.exports = { local_hour_to_gmt_hour, now, set_timezoneOffsetMinutes };
--- src/utils/time_impl.js
+++ src/utils/time_impl.js
@@ -1,7 +1,6 @@
 const moment = require("moment");
-//const printf = require('printf');
 
-const { log } = require("./logFile");
+//const { log } = require("./logFile");
 
 let timezoneOffsetMinutes = 0;
 let timezoneOffsetHours = 0;
@@ -10,29 +9,12 @@ function local_hour_to_gmt_hour(local_hour) {
   let gmt_hour = local_hour - timezoneOffsetHours;
   if (gmt_hour >= 24) gmt_hour -= 24;
   if (gmt_hour < 0) gmt_hour += 24;
-  log("local_hour_to_gmt_hour: offset = " + timezoneOffsetHours + ", local = " + local_hour + ", gmt = " + gmt_hour);
+  //log("local_hour_to_gmt_hour: offset = " + timezoneOffsetHours + ", local = " + local_hour + ", gmt = " + gmt_hour);
   return gmt_hour;
 }
 
 function now() {
   return moment().format("YYYY-MM-DD HH:mm:ss.SSSZ");
-  //return "YYYY-MM-DD HH:mm:ss.SSS";
-  /*
-  const date = new Date();
-  const date_string  = printf("%04d-%02d-%02d %02d:%02d:%02d.%03d",
-    date.getFullYear(),
-    date.getMonth() + 1,
-    date.getDate(),
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds(),
-    date.getMilliseconds());
-  return date_string;
-  */
-}
-
-function now_local() {
-  return moment().add(timezoneOffsetMinutes, 'minutes').format('MMMM Do YYYY, h:mm:ss a');
 }
 
 function set_timezoneOffsetMinutes(timezoneOffset) {
@@ -45,7 +27,7 @@ function set_timezoneOffsetMinutes(timezoneOffset) {
   if (neg) tzo_totalmins = -tzo_totalmins;
   timezoneOffsetMinutes = tzo_totalmins;
   timezoneOffsetHours = (neg ? -tzo_hrs : tzo_hours);
-  log("set_timezoneOffsetMinutes: mins = " + timezoneOffsetMinutes + ", hours = " + timezoneOffsetHours);
+  //log("set_timezoneOffsetMinutes: mins = " + timezoneOffsetMinutes + ", hours = " + timezoneOffsetHours);
 }
 
-module.exports = { local_hour_to_gmt_hour, now, now_local, set_timezoneOffsetMinutes };
+module.exports = { local_hour_to_gmt_hour, now, set_timezoneOffsetMinutes };
