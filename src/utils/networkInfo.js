b629d7fb262d9a163a969c67fe016592b3ec5402
ken Robesky <ken@robesky.org>
2023-05-02 19:54:34 -0700

    added sameSubnet

--- src/utils/networkInfo.js
+++ src/utils/networkInfo.js
@@ -65,4 +65,16 @@ async function getPublicIp(http) {
   return publicIPAddress;
 }
 
-module.exports = { getGatewayIp, getInterfaceName, getPrivateIp, getPublicIp };
+// NB: assumes ipv4
+function sameSubnet(ipAddress1, ipAddress2) {
+  log("sameSubnet: ipAddress1 = " + ipAddress1 + ", ipAddress2 = " + ipAddress2, "neti", "error");
+  const ip1 = ipAddress1.split('.');
+  const ip2 = ipAddress2.split('.');
+  for (let i = 0; i < ip1.length - 1; i++) {
+    //log("sameSubnet: ip1 = " + ip1[i] + ", ip2 = " + ip2[i], "neti", "error");
+    if (ip1[i] !== ip2[i]) return false;
+  }
+  return true;
+}
+
+module.exports = { getGatewayIp, getInterfaceName, getPrivateIp, getPublicIp, sameSubnet };
