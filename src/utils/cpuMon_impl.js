28c891f164564adf34cff39e2e75ba557b5e8012
ken Robesky <ken@robesky.org>
2023-05-17 13:08:19 -0700

    cpuMon - use prev sample in case of failure

--- src/utils/cpuMon_impl.js
+++ src/utils/cpuMon_impl.js
@@ -45,15 +45,17 @@ class CpuMon {
  
     try {
       // temp
+      // NB: Use prev sample in case of failure.
+      sample.temp_celsius = this.prev_sample.temp_celsius;
       const { stdout, stderr } = await spawnAsync("cat", ["/sys/devices/virtual/thermal/thermal_zone0/temp"]);
       if (stderr) {
         log("(Error) CpuMon.getCpuData - temp: " + stderr, "cpum", "error");
-      } else {
-        sample.temp_celsius = parseFloat((parseInt(stdout)/1000).toFixed(1));
-        if (sample.temp_celsius > 0)
-          this.prev_sample.temp_celsius = sample.temp_celsius;
-        else
-          sample.temp_celsius = this.prev_sample.temp_celsius;
+      } else if (stdout) {
+        const temp_celsius = parseFloat((parseInt(stdout)/1000).toFixed(1));
+        if (temp_celsius > 0) {
+          this.prev_sample.temp_celsius = temp_celsius;
+          sample.temp_celsius = temp_celsius;
+        }
       }
     } catch (ex) {
       log("(Exception) CpuMon.getCpuData - temp: " + ex, "cpum", "error"); 
@@ -61,24 +63,25 @@ class CpuMon {
 
     try {  
       // cpu utilization
+      sample.cpu_utlz_pct = this.prev_sample.cpu_utlz_pct;
       const { stdout, stderr } = await spawnAsync("iostat", ["-y", "-c"]);
       if (stderr) {
         log("(Error) CpuMon.getCpuData - utlz: " + stderr, "cpum", "error");
-      } else {
+      } else if (stdout) {
         //log("CpuMon.getCpuData: utilization = " + stdout);
         const lines = stdout.split('\n');
         const line = lines[3];
         const parts = line.replace(/\s\s+/g, ' ').split(' ');
-        const cpu_utlz_pct =  parseFloat(parts[1]) + // user
+        const cpu_utlz_pct_ = parseFloat(parts[1]) + // user
                               parseFloat(parts[2]) + // nice
                               parseFloat(parts[3]) + // system
                               parseFloat(parts[4]) + // iowait
                               parseFloat(parts[5]);  // steal
-        sample.cpu_utlz_pct = cpu_utlz_pct.toFixed(1);
-        if (sample.cpu_utlz_pct > 0)
-          this.prev_sample.cpu_utlz_pct = sample.cpu_utlz_pct;
-        else
-          sample.cpu_utlz_pct = this.prev_sample.cpu_utlz_pct;
+        const cpu_utlz_pct = cpu_utlz_pct_.toFixed(1);
+        if (cpu_utlz_pct > 0) {
+          this.prev_sample.cpu_utlz_pct = cpu_utlz_pct;
+          sample.cpu_utlz_pct = cpu_utlz_pct;
+        }
       }
     } catch (ex) {
       log("(Exception) CpuMon.getCpuData - cpu_utlz: " + ex, "cpum", "error"); 
@@ -86,6 +89,7 @@ class CpuMon {
 
     try {
       // memory
+      sample.mem_use_pct = this.prev_sample.mem_use_pct;
       const { stdout, stderr } = await spawnAsync("free", []);
       if (stderr) {
         log("(Error) CpuMon.getCpuData - free: " + stderr, "cpum", "error");
@@ -94,23 +98,21 @@ class CpuMon {
         //Mem:        3928784     3635720      186172       27540      106892      217036
         const rows = stdout.split("\n");
         const fields = rows[1].match(/\S+/g);
-        let total = parseInt(fields[1]);
-        let used = parseInt(fields[2]);
-        sample.mem_use_pct = ((used / total) * 100).toFixed(1);
-        if (sample.mem_use_pct > 0) {
-          log("CpuMon.getCpuData - free - saving cur: " + sample.mem_use_pct, "cpum", "info");
-          this.prev_sample.mem_use_pct = sample.mem_use_pct;
-        } else {
-          log("CpuMon.getCpuData - free - using prev: " + this.prev_sample.mem_use_pct, "cpum", "info");
-          sample.mem_use_pct = this.prev_sample.mem_use_pct;
+        const total = parseInt(fields[1]);
+        const used = parseInt(fields[2]);
+        const mem_use_pct = ((used / total) * 100).toFixed(1);
+        if (mem_use_pct > 0) {
+          this.prev_sample.mem_use_pct = mem_use_pct;
+          sample.mem_use_pct = mem_use_pct;
         }
-      }   
+      }  
     } catch (ex) {
       log("(Exception) CpuMon.getCpuData - free: " + ex, "cpum", "error");      
     }
 
     try {
       // storage
+      sample.stg_use_pct = this.prev_sample.stg_use_pct;
       const { stdout, stderr } = await spawnAsync("df", []);
       if (stderr) {
         log("(Error) CpuMon.getCpuData - df: " + stderr, "cpum", "error");
@@ -123,13 +125,13 @@ class CpuMon {
         //  cgroup           1964392       0   1964392   0% /sys/fs/cgroup
         const rows = stdout.split("\n");
         const fields = rows[3].match(/\S+/g);
-        let total = parseInt(fields[1]);
-        let used = parseInt(fields[2]);
-        sample.stg_use_pct = ((used / total) * 100).toFixed(1);
-        if (sample.stg_use_pct > 0)
-        this.prev_sample.stg_use_pct = sample.stg_use_pct;
-      else
-        sample.stg_use_pct = this.prev_sample.stg_use_pct;
+        const total = parseInt(fields[1]);
+        const used = parseInt(fields[2]);
+        const stg_use_pct = ((used / total) * 100).toFixed(1);
+        if (stg_use_pct > 0) {
+          this.prev_sample.stg_use_pct = stg_use_pct;
+          sample.stg_use_pct = stg_use_pct;
+        }
       }   
     } catch (ex) {
       log("(Exception) CpuMon.getCpuData - df: " + ex, "cpum", "error");      
